4d28673640d01cdc654c84ec1c6d3a6441d9f60a
Amir Mann <Amir-Mann@github.com>
2025-03-06 12:06:09 +0200

    Same as last but betters split into sections

--- main.py
+++ main.py
@@ -31,14 +31,78 @@ def generate_reads(genome, num_reads, read_length, error_prob):
     
     return reads
 
+
+def global_alignment(genome, sequence):
+    genome_length = len(genome)
+    log_genome_length = math.log(genome_length, 2)
+    
+    for i in range(genome_length):
+        count = 0
+        for j, c in enumerate(sequence):
+            if i + j >= genome_length:
+                break
+            count += int(c == genome[i + j])
+            if count - j > log_genome_length * 2: # Early stopping, see report
+                break
+        if count > len(sequence) / 3:
+            #print("match of length", len(sequence), "out of", genome_length, "with count", count)
+            recall = float(count) / genome_length
+            precision = float(count) / len(sequence)
+            iou = float(count) / (len(sequence) + genome_length - count)
+            return recall, precision, iou
+    return 0, 0, 0
+
+
+def iterative_test_sequencing(args, experiment_title, genome, num_reads, allow_mis_matches, testing_iterations=1, error_prob=0, print_results=True):
+    if not args.hide_progress_bar:
+        progress_bar = tqdm.tqdm(
+            total=testing_iterations,
+            desc="Processing",
+            bar_format="{l_bar}{bar} {n_fmt}/{total_fmt} | {elapsed} elapsed, {remaining} remaining, {rate_fmt}"
+        )
+
+    recall = 0
+    precision = 0
+    iou = 0
+    for _ in range(testing_iterations):
+        if not args.hide_progress_bar:
+            progress_bar.update(1)
+        error_reads = generate_reads(genome, num_reads, args.read_length, error_prob)
+        G = Graph()
+        with timer("create graph"):
+            G.load_from_strands(error_reads, allow_mis_matches)
+        
+        with timer("sequence"):
+            sequence = G.get_sequenced_result()
+        
+        with timer("eval"):
+            r, p, i = global_alignment(genome, sequence)
+        
+        recall += r / testing_iterations
+        precision += p / testing_iterations
+        iou += i / testing_iterations
+
+            
+    if not args.hide_progress_bar:
+        progress_bar.close()
+    
+    if print_results:
+        print()
+        print(experiment_title, "results:")
+        print(f"recall: {recall}")
+        print(f"precision: {precision}")
+        print(f"iou: {iou}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Generate error-prone reads from a genome.")
-    parser.add_argument("-f", "--fasta", type=str, default="demo.fasta", help="Path to the FASTA file containing the genome sequence.")
+    parser.add_argument("-f", "--fasta", type=str, default="sequence.fasta", help="Path to the FASTA file containing the genome sequence.")
     parser.add_argument("-l", "--read_length", type=int, default=100, help="Length of each read (default: 100).")
     parser.add_argument("-c", "--coverage", type=float, default=50.0, help="Desired coverage (default: 50x).")
     parser.add_argument("-p", "--error_prob", type=float, default=0.01, help="Base mismatch probability (default: 0.01).")
-    parser.add_argument("-i", "--testing_iterations", type=int, default=1000, help="Number of iterations to test the quality of the algorithm.")
-    parser.add_argument("-a", "--allow_mis_matches", type=str, choices=["0", "1", "2", "3", "log"], default=1000, help="How many miss matches to allow when comparing suffix to prefix.")
+    parser.add_argument("-i", "--testing_iterations", type=int, default=1000, help="Number of iterations to test the quality of the algorithm. Only for erroneous reads")
+    parser.add_argument("-a", "--allow_mis_matches", type=str, choices=["0", "1", "2", "3", "log"], default="1", 
+                        help="How many miss matches to allow when comparing suffix to prefix. Only for erroneous reads")
 
     parser.add_argument("--hide_progress_bar", action="store_true", help="Hides the progress bar")
     parser.add_argument("--hide_timing", action="store_true", help="Hides the timing resultsprogress bar")
@@ -49,74 +113,22 @@ def main():
     
     genome = read_fasta(args.fasta)
     genome_length = len(genome)
-    log_genome_length = math.log(genome_length, 2)
     
     num_reads = int((args.coverage * genome_length) / args.read_length)
     print(f"Generating {num_reads} reads.")
-    
-    clean_reads = generate_reads(genome, num_reads, args.read_length, 0)
 
     if args.allow_mis_matches == "log":
         allow_mis_matches = int(math.log(args.read_length, 4))
     else:
         allow_mis_matches = int(args.allow_mis_matches)
     
-    G = Graph()
-    G.load_from_strands(clean_reads, allow_mis_matches)
-    sequence = G.get_sequenced_result()
-    for i in range(30):
-        if sequence == genome[i:i+len(sequence)]:
-            print("match of length", len(sequence), "out of", len(genome))
-            break
-    else:
-        print("No match")
+    iterative_test_sequencing(args, "Clean reads", genome, num_reads, allow_mis_matches=0)
     
-
-    if not args.hide_progress_bar:
-        progress_bar = tqdm.tqdm(
-            total=args.testing_iterations,
-            desc="Processing",
-            bar_format="{l_bar}{bar} {n_fmt}/{total_fmt} | {elapsed} elapsed, {remaining} remaining, {rate_fmt}"
-        )
     start = time.time()
     
-    recall = 0
-    precision = 0
-    iou = 0
-    for _ in range(args.testing_iterations):
-        if not args.hide_progress_bar:
-            progress_bar.update(1)
-        error_reads = generate_reads(genome, num_reads, args.read_length, args.error_prob)
-        G = Graph()
-        with timer("create graph"):
-            G.load_from_strands(error_reads, allow_mis_matches)
-        
-        with timer("sequence"):
-            sequence = G.get_sequenced_result()
-        
-        with timer("eval"):
-            for i in range(genome_length):
-                count = 0
-                for j, c in enumerate(sequence):
-                    if i + j >= genome_length:
-                        break
-                    count += int(c == genome[i + j])
-                    if count - j > log_genome_length * 2: # Early stopping, see report
-                        break
-                if count > len(sequence) / 3:
-                    #print("match of length", len(sequence), "out of", genome_length, "with count", count)
-                    recall += float(count) / args.testing_iterations / genome_length
-                    precision += float(count) / args.testing_iterations / len(sequence)
-                    iou += float(count) / (len(sequence) + genome_length - count) / args.testing_iterations
-                    break
-    if not args.hide_progress_bar:
-        progress_bar.close()
-    
-    print()
-    print(f"recall: {recall}")
-    print(f"precision: {precision}")
-    print(f"iou: {iou}")
-    
+    iterative_test_sequencing(args, f"With error probability {args.error_prob}", genome, num_reads, allow_mis_matches=allow_mis_matches,
+                              testing_iterations=args.testing_iterations, error_prob=args.error_prob)
+
     total = time.time() - start
     if not args.hide_timing:
         print(f"took {total:.2f} seconds to run")
