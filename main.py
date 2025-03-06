6184a589d9ee84e494e8671e47aea21834356828
Amir Mann <Amir-Mann@github.com>
2025-03-06 11:54:36 +0200

    working version before prettification

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import math
+import time
+import tqdm
 import random
 import argparse
-from strands_graph import Graph
+from strands_graph import Graph, timings, timer
 
 
 def read_fasta(filename):
@@ -28,39 +31,97 @@ def generate_reads(genome, num_reads, read_length, error_prob):
     
     return reads
 
-
 def main():
     parser = argparse.ArgumentParser(description="Generate error-prone reads from a genome.")
     parser.add_argument("-f", "--fasta", type=str, default="demo.fasta", help="Path to the FASTA file containing the genome sequence.")
     parser.add_argument("-l", "--read_length", type=int, default=100, help="Length of each read (default: 100).")
     parser.add_argument("-c", "--coverage", type=float, default=50.0, help="Desired coverage (default: 50x).")
     parser.add_argument("-p", "--error_prob", type=float, default=0.01, help="Base mismatch probability (default: 0.01).")
+    parser.add_argument("-i", "--testing_iterations", type=int, default=1000, help="Number of iterations to test the quality of the algorithm.")
+    parser.add_argument("-a", "--allow_mis_matches", type=str, choices=["0", "1", "2", "3", "log"], default=1000, help="How many miss matches to allow when comparing suffix to prefix.")
+
+    parser.add_argument("--hide_progress_bar", action="store_true", help="Hides the progress bar")
+    parser.add_argument("--hide_timing", action="store_true", help="Hides the timing resultsprogress bar")
+    parser.add_argument("-s", "--seed", type=int, default=7, help="The seed to run the program with")
     args = parser.parse_args()
+
+    random.seed(args.seed)
     
     genome = read_fasta(args.fasta)
     genome_length = len(genome)
+    log_genome_length = math.log(genome_length, 2)
     
     num_reads = int((args.coverage * genome_length) / args.read_length)
+    print(f"Generating {num_reads} reads.")
     
     clean_reads = generate_reads(genome, num_reads, args.read_length, 0)
-    
-    error_reads = generate_reads(genome, num_reads, args.read_length, args.error_prob)
+
+    if args.allow_mis_matches == "log":
+        allow_mis_matches = int(math.log(args.read_length, 4))
+    else:
+        allow_mis_matches = int(args.allow_mis_matches)
     
     G = Graph()
-    G.load_from_strands(clean_reads)
+    G.load_from_strands(clean_reads, allow_mis_matches)
     sequence = G.get_sequenced_result()
-    i = 0
-    step = 20
-    while i + step < len(genome) and i + step < len(sequence):
-        print(f"org: {genome[i: i+step]}")
-        print(f"seq: {sequence[i: i+step]}, {sequence[i: i+step]==genome[i: i+step]}")
-        i = i + step
-    print(f"org: {genome[i:]}")
-    print(f"seq: {sequence[i:]}")
+    for i in range(30):
+        if sequence == genome[i:i+len(sequence)]:
+            print("match of length", len(sequence), "out of", len(genome))
+            break
+    else:
+        print("No match")
+    
 
-    """print(f"Generated {num_reads} reads for {args.coverage}x coverage with error probability {args.error_prob}.")
-    print(f"Min clean read is {min(clean_reads)}")
-    print(f"Min error read is {min(error_reads)}")"""
+    if not args.hide_progress_bar:
+        progress_bar = tqdm.tqdm(
+            total=args.testing_iterations,
+            desc="Processing",
+            bar_format="{l_bar}{bar} {n_fmt}/{total_fmt} | {elapsed} elapsed, {remaining} remaining, {rate_fmt}"
+        )
+    start = time.time()
+    
+    recall = 0
+    precision = 0
+    iou = 0
+    for _ in range(args.testing_iterations):
+        if not args.hide_progress_bar:
+            progress_bar.update(1)
+        error_reads = generate_reads(genome, num_reads, args.read_length, args.error_prob)
+        G = Graph()
+        with timer("create graph"):
+            G.load_from_strands(error_reads, allow_mis_matches)
+        
+        with timer("sequence"):
+            sequence = G.get_sequenced_result()
+        
+        with timer("eval"):
+            for i in range(genome_length):
+                count = 0
+                for j, c in enumerate(sequence):
+                    if i + j >= genome_length:
+                        break
+                    count += int(c == genome[i + j])
+                    if count - j > log_genome_length * 2: # Early stopping, see report
+                        break
+                if count > len(sequence) / 3:
+                    #print("match of length", len(sequence), "out of", genome_length, "with count", count)
+                    recall += float(count) / args.testing_iterations / genome_length
+                    precision += float(count) / args.testing_iterations / len(sequence)
+                    iou += float(count) / (len(sequence) + genome_length - count) / args.testing_iterations
+                    break
+    if not args.hide_progress_bar:
+        progress_bar.close()
+    
+    print()
+    print(f"recall: {recall}")
+    print(f"precision: {precision}")
+    print(f"iou: {iou}")
+    
+    total = time.time() - start
+    if not args.hide_timing:
+        print(f"took {total:.2f} seconds to run")
+        for key, value in timings.items():
+            print(f"{key} was {value / total * 100:.2f}% of runtime")
     
 
 if __name__ == "__main__":
--- strands_graph.py
+++ strands_graph.py
@@ -1,5 +1,20 @@
 
 
+import math
+import time
+from contextlib import contextmanager
+
+
+timings = {}
+@contextmanager
+def timer(key):
+    start = time.time()
+    yield
+    if key not in timings:
+        timings[key] = 0
+    timings[key] += time.time() - start
+
+
 class Vertex:
     def __init__(self, sequence: str) -> None:
         self.sequence: str = sequence
@@ -7,17 +22,52 @@ class Vertex:
 
 
 class TrieNode:
-    def __init__(self) -> None:
+    def __init__(self, extra=None, count=0) -> None:
         self.A: 'TrieNode' = None
         self.C: 'TrieNode' = None
         self.G: 'TrieNode' = None
         self.T: 'TrieNode' = None
-        self.count: int = 0
-        self.extra: Vertex | None = None
+        self.count: int = count
+        self.extra: Vertex | None = extra
 
-    def insert_strand(self, strand: str, extra: Vertex | None = None) -> Vertex | None:
+    def str_aux(self):
+        sub_tree = []
+        lines = 0
+        if self.A is not None:
+            A_sub_tree, A_lines = self.A.str_aux()
+            sub_tree.append("-\033[30;105mA\033[0m" + A_sub_tree[0])
+            sub_tree.extend([" \\" + line for line in A_sub_tree[1:]])
+            #sub_tree.append(" " * len(sub_tree[-1]))
+            lines += A_lines
+        if self.C is not None:
+            C_sub_tree, C_lines = self.C.str_aux()
+            sub_tree.append("-\033[30;105mC\033[0m" + C_sub_tree[0])
+            sub_tree.extend([" \\" + line for line in C_sub_tree[1:]])
+            #sub_tree.append(" " * len(sub_tree[-1]))
+            lines += C_lines
+        if self.G is not None:
+            G_sub_tree, G_lines = self.G.str_aux()
+            sub_tree.append("-\033[30;105mG\033[0m" + G_sub_tree[0])
+            sub_tree.extend([" \\" + line for line in G_sub_tree[1:]])
+            #sub_tree.append(" " * len(sub_tree[-1]))
+            lines += G_lines
+        if self.T is not None:
+            T_sub_tree, T_lines = self.T.str_aux()
+            sub_tree.append("-\033[30;105mT\033[0m" + T_sub_tree[0])
+            sub_tree.extend([" \\" + line for line in T_sub_tree[1:]])
+            #sub_tree.append(" " * len(sub_tree[-1]))
+            lines += T_lines
+        if lines == 0:
+            lines += 1
+            sub_tree = [""]
+        return sub_tree, lines
+    
+    def __str__(self):
+        return "\n".join(self.str_aux()[0])
+
+    def insert_strand(self, strand: str, extra: Vertex | None = None, new_extra: bool = False) -> Vertex | None:
+        #print(f"{len(strand)=}")
         self.count += 1
-        new_extra = False
         if self.extra is None:
             self.extra = extra if extra is not None else Vertex(strand)
             new_extra = True
@@ -27,18 +77,29 @@ class TrieNode:
         
         first_char = strand[0]
         next_node: TrieNode | None = getattr(self, first_char)
-        if next_node is None:
-            next_node = TrieNode()
-            setattr(self, first_char, next_node)
+        if next_node is None: # Opening recursion for faster runtimes.
+            current = self
+            extra = self.extra if new_extra else Vertex(strand)
+            for c in strand:
+                next_node = TrieNode(extra, 1)
+                setattr(current, c, next_node)
+                current = next_node
+            return extra
         
-        return next_node.insert_strand(strand[1:], self.extra)
+        return next_node.insert_strand(strand[1:], self.extra if new_extra else None, new_extra=new_extra)
 
-    def search(self, strand: str) -> Vertex | None:
+    def search(self, strand: str, allow_mis_matches=0) -> Vertex | None:
         node = self
-        for char in strand:
+        for i, char in enumerate(strand):
             next_node: TrieNode | None = getattr(node, char)
             if next_node is None:
-                return node.extra
+                if not allow_mis_matches:
+                    return None
+                for char in "ACGT":
+                    extra = node.search(char + strand[i+1:], allow_mis_matches=allow_mis_matches - 1)
+                    if extra is not None:
+                        return extra
+                return None
             node = next_node
         return node.extra
 
@@ -47,49 +108,56 @@ class Graph:
     def __init__(self) -> None:
         self.vertices: dict[Vertex, None] = {}
 
-    def load_from_strands(self, strands: list[str]) -> None:
+    def load_from_strands(self, strands: list[str], allow_mis_matches) -> None:
         trie = TrieNode()
         
         for strand in strands:
-            vertex = trie.insert_strand(strand)
+            with timer("insert_strand"):
+                vertex = trie.insert_strand(strand)
             if vertex is not None:
                 vertex.sequence = strand
                 self.vertices[vertex] = None
         
         for vertex in self.vertices.keys():
-            reverse_strand = vertex.sequence[::-1]
-            best_match = trie.search(reverse_strand)
-            if best_match and best_match != vertex:
-                vertex.connected_vertices.append(best_match)
+            for i in range(1, len(vertex.sequence) - 2 * int(math.log(len(strands), 4))):
+                suffix_strand = vertex.sequence[i:]
+                with timer("search"):
+                    match = trie.search(suffix_strand, allow_mis_matches)
+                if match and match != vertex:
+                    vertex.connected_vertices.append((match, len(vertex.sequence) - i))
+                    break
+        
+        #print(trie)
 
-    def get_sequenced_result(self) -> str:
-        incoming_links = {v: False for v in self.vertices.keys()}
+    def get_sequenced_result(self, min_overlap=7, in_coming_links_min=40) -> str:
+        incoming_links_values = {v: 0 for v in self.vertices.keys()}
+        #print(f"{len(self.vertices.keys())=}")
         
         for vertex in self.vertices.keys():
-            for connected in vertex.connected_vertices:
-                incoming_links[connected] = True
-        
-        for origin, really_an_origin in incoming_links.items():
-            if not really_an_origin:
-                break
-        else:
-            print("Error no origin")
-            return ""
-        
-        current = origin
-        sequence = current.sequence
-        
-        while current.connected_vertices:
-            next_vertex = current.connected_vertices[0]
-            
-            max_overlap = 0
-            for i in range(1, min(len(current.sequence), len(next_vertex.sequence)) + 1):
-                if current.sequence[-i:] == next_vertex.sequence[:i]:
-                    max_overlap = i
-                else:
+            for edge_end, match_length in vertex.connected_vertices:
+                incoming_links_values[edge_end] += match_length
+
+        max_seq = ""
+
+        #print(incoming_links_values[special_vert])
+        for origin, sum_incoming_links in incoming_links_values.items():
+            if sum_incoming_links > in_coming_links_min:
+                continue
+                
+            #print(f"{origin.sequence=}")
+            current = origin
+            sequence = current.sequence
+            i = 0
+            while current.connected_vertices and i < len(self.vertices):
+                next_vertex, overlap_size = current.connected_vertices[0]
+                if overlap_size < min_overlap:
                     break
+                
+                sequence += next_vertex.sequence[overlap_size:]
+                current = next_vertex
+                i += 1
             
-            sequence += next_vertex.sequence[max_overlap:]
-            current = next_vertex
-        
-        return sequence
+            if len(sequence) > len(max_seq):
+                max_seq = sequence
+
+        return max_seq#
